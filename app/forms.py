0b2841799776bddd404ca48888df057f8e6f30e7
SkirdaMP <skirdamp@gmail.com>
2020-02-18 21:11:02 +0300

    first commit

---
+++ app/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+	"""docstring for LoginForm"""
+	username = StringField('Логин', validators = [DataRequired()])
+	password = PasswordField('Пароль', validators = [DataRequired()])
+	remember_me = BooleanField('Запомнить меня')
+	submit = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Логин', validators = [DataRequired()])
+	email = StringField('Email', validators = [DataRequired(), Email()])
+	password = PasswordField('Пароль', validators = [DataRequired()])
+	password2 = PasswordField('Повторите пароль', 
+					validators = [DataRequired(), EqualTo('password')])
+	submit = SubmitField('Зарегистрироваться')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username = username.data).first()
+		if user is not None:
+			raise ValidationError('Please, use another username.')
+
+	def validate_email(self, email):
+		user_mail = User.query.filter_by(email = email.data).first()
+		if user_mail is not None:
+			raise ValidationError('Please, use another email address.')
+
+
+class EditProfileForm(FlaskForm):
+	username = StringField('Логин', validators = [DataRequired()])
+	about_me = TextAreaField('О себе', validators = [DataRequired(), Length(min = 0, max = 200)])
+	submit = SubmitField('Подтвердить')
+
+	def __init__(self, original_username, *args, **kwargs):
+		super(EditProfileForm, self).__init__(*args, **kwargs)
+		self.original_username = original_username
+
+	def validate_username(self, username):
+		if username.data != self.original_username:
+			user = User.query.filter_by(username = self.username.data).first()
+			if user is not None:
+				raise ValidationError('Данное имя уже занято. Используйте другое.')
